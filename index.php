2eecf5e69a5e67ff311eba72fa4cae45eda47b27
frostkom <martin.fors@frostkom.se>
2016-09-21 09:37:16 +0200

    Standardize code amongst all plugins

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Custom Login
 Plugin URI: https://github.com/frostkom/mf_custom_login
 Description: 
-Version: 1.2.12
+Version: 1.2.13
 Author: Martin Fors
 Author URI: http://frostkom.se
 Text Domain: lang_login
