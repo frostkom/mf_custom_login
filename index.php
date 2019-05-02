4cbc9a6c0706e1b5e85f10ae25661b6dc56022d8
frostkom <martin.fors@frostkom.se>
2019-05-02 08:59:15 +0200

    Update translations

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Custom Login
 Plugin URI: https://github.com/frostkom/mf_custom_login
 Description: 
-Version: 2.7.11
+Version: 2.7.12
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
