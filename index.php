8f558eb25a7eb0f292a0d5f675fef6e5f3bb6a40
frostkom <martin.fors@frostkom.se>
2025-02-05 10:22:26 +0100

    Make script load in footer

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Custom Login
 Plugin URI: https://github.com/frostkom/mf_custom_login
 Description:
-Version: 3.5.21
+Version: 3.5.22
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://martinfors.se
